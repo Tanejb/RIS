a7842a48e3e03db3a82405c56f054803e098d328
miha.prah <mihaprah@Mihas-MacBook-Pro-2.local>
2022-12-01 17:16:34 +0100

    vzpostavitev baze #RIS1-12 DONE Time- 1h

--- ris/src/main/java/com/example1/ris/controllers/InstruktorController.java
+++ ris/src/main/java/com/example1/ris/controllers/InstruktorController.java
@@ -1,9 +1,13 @@
 package com.example1.ris.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/instruktorji")
 public class InstruktorController {
+
+    @Autowired
+    private InstruktorController instruktorDao;
 }
