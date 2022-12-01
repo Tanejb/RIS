aff7e5efbf4eb60504df74a7f1e7567a79c907b2
mihaprah <mihaprah@Mihas-MacBook-Pro-2.local>
2022-12-01 16:47:57 +0100

    vzpostavitev baze #RIS1-12 DONE Time- 1h

---
+++ ris/src/main/java/com/example1/ris/controllers/InstruktorController.java
@@ -0,0 +1,9 @@
+package com.example1.ris.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/instruktorji")
+public class InstruktorController {
+}
