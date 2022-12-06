887ea6fbf5ff3db10f1ac70f2448dc8e9209292d
miha.prah <mihaprah@Mihas-MacBook-Pro-2.local>
2022-12-06 21:49:13 +0100

    dodajanje GET metod #RIS1-15 DONE Time- 1h

--- ris/src/main/java/com/example1/ris/controllers/InstruktorController.java
+++ ris/src/main/java/com/example1/ris/controllers/InstruktorController.java
@@ -4,8 +4,11 @@ import com.example1.ris.dao.InstruktorRepository;
 import com.example1.ris.models.Instruktor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/instruktorji")
@@ -13,5 +16,13 @@ public class InstruktorController {
 
     @Autowired
     private InstruktorRepository instruktorDao;
-    
+
+    @GetMapping
+    public Iterable<Instruktor> vrniInstruktorje(){return instruktorDao.findAll();}
+
+    @GetMapping("/{instruktor_id}")
+    public Optional<Instruktor> vrniInstruktorjaPoId(@PathVariable(name = "instruktor_id")Long instruktor_id){
+        return instruktorDao.findById(instruktor_id);
+    }
+
 }
