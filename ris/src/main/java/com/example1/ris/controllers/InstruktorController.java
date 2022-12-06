59befb38de49079c6e51eb93268c610e9c5035fd
miha.prah <mihaprah@Mihas-MacBook-Pro-2.local>
2022-12-06 22:23:12 +0100

    dodajanje POST metod #RIS1-16 DONE Time-1h

--- ris/src/main/java/com/example1/ris/controllers/InstruktorController.java
+++ ris/src/main/java/com/example1/ris/controllers/InstruktorController.java
@@ -1,12 +1,14 @@
 package com.example1.ris.controllers;
 
+import com.example1.ris.dao.AvtosolaRepository;
 import com.example1.ris.dao.InstruktorRepository;
+import com.example1.ris.dao.TerminRepository;
+import com.example1.ris.exceprion.ResourceNotFoundException;
+import com.example1.ris.models.Avtosola;
 import com.example1.ris.models.Instruktor;
+import com.example1.ris.models.Termin;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -17,12 +19,49 @@ public class InstruktorController {
     @Autowired
     private InstruktorRepository instruktorDao;
 
+    @Autowired
+    private AvtosolaRepository avtosolaDao;
+
+    @Autowired
+    private TerminRepository terminDao;
+
     @GetMapping
     public Iterable<Instruktor> vrniInstruktorje(){return instruktorDao.findAll();}
 
     @GetMapping("/{instruktor_id}")
     public Optional<Instruktor> vrniInstruktorjaPoId(@PathVariable(name = "instruktor_id")Long instruktor_id){
         return instruktorDao.findById(instruktor_id);
+    }
+
+    @PostMapping
+    public Instruktor dodajInstruktorja(@RequestBody Instruktor instruktor){
+        return instruktorDao.save(instruktor);
+    }
+
+    // Dodaj Avtošolo Instrukturju
+    @PostMapping("/dodajAvtosolo/{instruktor_id}")
+    public Instruktor dodajAvtosoloInstrukturju(@PathVariable(name = "instruktor_id")Long instruktor_id, @RequestBody Avtosola avtosola){
+        Instruktor iskanInstruktor = instruktorDao.findById(instruktor_id).orElseThrow(() -> new ResourceNotFoundException("Instruktor ne obstaja z id: " + instruktor_id));
+        iskanInstruktor.setAvtosola(avtosola);
+
+        Avtosola dodanaAvtosola = avtosolaDao.findById(avtosola.getId()).orElseThrow(() -> new ResourceNotFoundException("Avtosola ne obstaja z id: " + avtosola.getId()));
+        dodanaAvtosola.getInstruktorji().add(iskanInstruktor);
+        avtosolaDao.save(dodanaAvtosola);
+
+        return instruktorDao.save(iskanInstruktor);
+    }
+
+    // Dodaj Termin Instrukturju
+    @PostMapping("/dodajTermin/{instruktor_id}")
+    public Instruktor dodajTerminInstrukturju(@PathVariable(name = "instruktor_id")Long instruktor_id, @RequestBody Termin termin){
+        Instruktor iskanInstruktor = instruktorDao.findById(instruktor_id).orElseThrow(()  -> new ResourceNotFoundException("Instruktor ne obstaja z id: " + instruktor_id));
+        iskanInstruktor.getTermini().add(termin);
+
+        Termin dodanTermin = terminDao.findById(termin.getId()).orElseThrow(()  -> new ResourceNotFoundException("Termin ne obstaja z id: " + termin.getId()));
+        dodanTermin.setInstruktor(iskanInstruktor);
+        terminDao.save(dodanTermin);
+
+        return instruktorDao.save(iskanInstruktor);
     }
 
 }
---
+++ ris/src/main/java/com/example1/ris/dao/TerminRepository.java
@@ -0,0 +1,7 @@
+package com.example1.ris.dao;
+
+import com.example1.ris.models.Termin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TerminRepository extends CrudRepository<Termin, Long> {
+}
