4f017ac77e82a6c21516cbd9e9dbc17e259b9af7
miha.prah <mihaprah@Mihas-MacBook-Pro-2.local>
2022-12-12 22:20:16 +0100

    T1 implementacija posiljanja mailov #RIS1-26 DONE Time- 30m

--- ris/src/main/java/com/example1/ris/RisApplication.java
+++ ris/src/main/java/com/example1/ris/RisApplication.java
@@ -1,13 +1,25 @@
 package com.example1.ris;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class RisApplication {
+
+	@Autowired
+	private EmailSenderService emailSenderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RisApplication.class, args);
 	}
 
+//		@EventListener(ApplicationReadyEvent.class)
+//		public void sendEmail(){
+//			String welcome = "Welcome, Miha";
+//			String body = "Thank you for registering on MojInstruktor app!";
+//			emailSenderService.sendEmail("prah.miha1@gmail.com", welcome, body);
+//		}
 }
